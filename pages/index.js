890013840b1cb0546eb6dcea40f652a5f630a234
riley-han <rileyhansen29@gmail.com>
2022-05-01 22:13:16 -0600

    installing tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,11 @@
 import React from "react"
-import styled from "./styled-components"
 
 const LandingPage = () => {
   return (
     <div>
-      <button>x</button>
+      <h1>hello world</h1>
     </div>
   )
 }
 
-const Layout = styled.div`
-  width: 100%;
-`
 export default LandingPage
